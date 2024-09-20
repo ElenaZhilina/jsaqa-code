d118463382f7d292c49abb4457540fff7761b5ee
Zhilina Elena <elenazhilina@yandex.ru>
2024-09-20 21:41:51 +0500

    Jest copmleted

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -2,16 +2,35 @@ const sorting = require("../../app");
 
 describe("Books names test suit", () => {
   it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
+    const input = [
+      "Гарри Поттер",
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+    ];
+
+    const expected = [
       "Властелин Колец",
       "Волшебник изумрудного города",
       "Гарри Поттер",
-    ]);
+    ];
+
+    const output = sorting.sortByName(input);
+
+    expect(output).toEqual(expected);
   });
+
+  it("should return an empty array when input is empty", () => {
+    const input = [];
+    const expected = [];
+    const output = sorting.sortByName(input);
+    expect(output).toEqual(expected);
+  });
+
+  it("should handle duplicate names", () => {
+    const input = ["Гарри Поттер", "Гарри Поттер", "Властелин Колец"];
+    const expected = ["Властелин Колец", "Гарри Поттер", "Гарри Поттер"];
+    const output = sorting.sortByName(input);
+    expect(output).toEqual(expected);
+  });
+
 });
